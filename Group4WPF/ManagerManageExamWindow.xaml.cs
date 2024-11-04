f9430fa6a5b852772949eaad0b6dfad232f81ca6
xHexed <58384565+xHexed@users.noreply.github.com>
2024-11-04 21:26:34 +0700

    fix manage exam window data on load

--- Group4WPF/ManagerManageExamWindow.xaml.cs
+++ Group4WPF/ManagerManageExamWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BLL;
+using BOs;
 using Group4WPF;
 using System;
 using System.Collections.Generic;
@@ -22,19 +23,23 @@ namespace WpfApp
     public partial class ManagerManageExamWindow : Window
     {
         private readonly Window _window;
-        private readonly SemesterService semesterService;
+        private readonly AccountService accountService;
         private readonly CourseService courseService;
         private readonly RoomService roomService;
         private readonly SlotService slotService;
+        private readonly ScheduleService scheduleService;
+        private readonly Schedule _schedule;
 
-        public ManagerManageExamWindow(Window window)
+        public ManagerManageExamWindow(Window window, Schedule schedule)
         {
             InitializeComponent();
             _window = window;
-            semesterService = new SemesterService();
+            _schedule = schedule;
+            accountService = new AccountService();
             courseService = new CourseService();
             roomService = new RoomService();
             slotService = new SlotService();
+            scheduleService = new ScheduleService();
         }
 
         private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -44,22 +49,45 @@ namespace WpfApp
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            SemesterComboBox.ItemsSource = semesterService.GetSemesters();
-            List<String> status = ["Enabled", "Disabled"];
+            List<Account> accounts = accountService.GetAccounts();
+            AccountComboBox.ItemsSource = accounts;
+            AccountComboBox.SelectedIndex = accounts.FindIndex(0, 1, (account) => account.AccountId == _schedule.AccountId);
+
+            List<string> status = ["Enabled", "Disabled"];
             StatusComboBox.ItemsSource = status;
-            CourseComboBox.ItemsSource = courseService.GetCourses();
-            RoomComboBox.ItemsSource = roomService.GetRooms();
-            SlotComboBox.ItemsSource = slotService.GetSlots();
+            StatusComboBox.SelectedIndex = _schedule.Status;
+
+            List<Course> courses = courseService.GetCourses();
+            CourseComboBox.ItemsSource = courses;
+            CourseComboBox.SelectedIndex = courses.FindIndex(0, 1, (course) => course.CourseId == _schedule.CourseId);
+
+            List<Room> rooms = roomService.GetRooms();
+            RoomComboBox.ItemsSource = rooms;
+            RoomComboBox.SelectedIndex = rooms.FindIndex(0, 1, (room) => room.RoomId == _schedule.RoomId);
+
+            List<Slot> slots = slotService.GetSlots();
+            SlotComboBox.ItemsSource = slots;
+            SlotComboBox.SelectedItem = slots.FindIndex(0, 1, (slot) => slot.SlotId == _schedule.SlotId);
+
+            ScheduleDatepicker.SelectedDate = _schedule.ScheduleDate;
         }
 
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
-            //TODO: Exam
+            _schedule.AccountId = ((Account)AccountComboBox.SelectedItem).AccountId;
+            _schedule.Status = (byte)StatusComboBox.SelectedIndex;
+            _schedule.CourseId = ((Course)CourseComboBox.SelectedItem).CourseId;
+            _schedule.RoomId = ((Room)RoomComboBox.SelectedItem).RoomId;
+            _schedule.SlotId = ((Slot)SlotComboBox.SelectedItem).SlotId;
+            scheduleService.UpdateScheduled(_schedule);
         }
 
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            if (Util.TryDelete(() =>
+            {
+                scheduleService.DeleteScheduled(_schedule.ScheduleId);
+            })) ButtonCancel_Click(sender, e);
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
